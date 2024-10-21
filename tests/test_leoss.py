d94d70e641673cdb4a55a3418ec2bd0639fd7ded
space-hiro <144877753+space-hiro@users.noreply.github.com>
2024-10-21 23:14:11 +0800

    v0.3.5 update version, pass all previous tests, upload to pyPI

--- tests/test_leoss.py
+++ tests/test_leoss.py
@@ -3,7 +3,7 @@ from leoss import *
 
 
 def test__00__ShowsCorrectVersion():
-    assert __version__ == "0.3.4"
+    assert __version__ == "0.3.5"
 
 def test__01__SPACECRAFT_AddingAndListingToLEOSSsIsWorking():
     '''
